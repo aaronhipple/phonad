848dd8cb848bde8988d971d91e483a3e60c214cd
Aaron Hipple <ahipple@gmail.com>
2017-06-10 13:16:31 -0600

    Add arithmetic trait (#7)
    * Add arithmetic trait
    
    * Style fixup
    
    * Ignore some stuff in codeclimate for arithmetic
    
    * Fix up some PHP5.6 grumpiness
    
    * Add modulo operation
    
    * Make phpunit coverage happy

---
+++ src/Traits/Arithmetic.php
@@ -0,0 +1,119 @@
+<?php namespace Phonad\Traits;
+
+/**
+ * Arithmetic provides some mathematical operations.
+ */
+trait Arithmetic
+{
+    /**
+     * add returns a callback for addition.
+     *
+     * @param $x number
+     */
+    protected static function add($x)
+    {
+        /**
+         * @param $y number
+         */
+        return function ($y) use ($x) {
+            return $x + $y;
+        };
+    }
+
+    /**
+     * subtractFrom returns a callback for subtraction.
+     *
+     * @param $x number
+     */
+    protected static function subtractFrom($x)
+    {
+        /**
+         * @param $y number
+         */
+        return function ($y) use ($x) {
+            return $x - $y;
+        };
+    }
+
+    /**
+     * subtract returns a callback for subtraction.
+     *
+     * @param $y number
+     */
+    protected static function subtract($y)
+    {
+        /**
+         * @param $x number
+         */
+        return function ($x) use ($y) {
+            $subtractFrom = static::subtractFrom($x);
+            return $subtractFrom($y);
+        };
+    }
+
+    /**
+     * multiply returns a callback for multiplication.
+     *
+     * @param $x number
+     */
+    protected static function multiply($x)
+    {
+        /**
+         * @param $y number
+         */
+        return function ($y) use ($x) {
+            return $x * $y;
+        };
+    }
+
+    /**
+     * divide returns a callback for division.
+     *
+     * @param $x number
+     */
+    protected static function divide($x)
+    {
+        /**
+         * @param $y number
+         */
+        return function ($y) use ($x) {
+            return $y !== 0
+                ? $x / $y
+                : null;
+        };
+    }
+
+    /**
+     * divideBy returns a callback for division.
+     *
+     * @param $y number
+     */
+    protected static function divideBy($y)
+    {
+        /**
+         * @param $x number
+         */
+        return function ($x) use ($y) {
+            $divideBy = static::divide($x);
+            return $divideBy($y);
+        };
+    }
+
+    /**
+     * modulo returns a callback for modulo.
+     *
+     * @param $y number
+     */
+    protected static function modulo($y)
+    {
+        /**
+         * @param $x number
+         */
+        return function ($x) use ($y) {
+            if ($y === 0) {
+                return null;
+            }
+            return $x % $y;
+        };
+    }
+}
